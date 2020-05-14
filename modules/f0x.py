8edd33f92c1fce5f5ac37e7a5e9fa2d1fa20aa76
Dinesh Saini <dinesh.saini.277@gmail.com>
2020-05-15 02:41:34 +0530

    arg parsed

--- modules/f0x.py
+++ modules/f0x.py
@@ -33,80 +33,86 @@ class f0x(BaseModule):
     
     def defineArgs(self):
         self.getParser().add_argument('-s', '--site', help = \
-                'Specify target site.', dest='site')
+                'Specify target site.', dest = 'site', metavar = 'target')
 
         self.getParser().add_argument('-q', '--query', help = \
-                'Dork to use. If specified, other files will not be read.', \
-                dest='query')
+                'Use this dork, instead of reading from repo files.', \
+                dest = 'query', metavar = 'dork')
         
-        self.getParser().add_argument('-i', '--inclusive', help = \
-                'This works with `query` option only, if used, will \
-                also read dorks from file. ', dest='inc', action="store_true")
+        self.getParser().add_argument('-i', '--inclusive', help = 'Read from \
+                repo files for dorks, along with the dork supplied via \
+                \'query\' argument. Required \'query\' argument to be \
+                present.', dest='inc', action="store_true")
 
         self.getParser().add_argument('-A', '--args', help = \
                 'Specify extra query to supply with each dorks.', \
-                dest='ex_query')
+                dest='ex_query', metavar='query')
 
         self.getParser().add_argument('-C', '--category', help = \
-                'Use dorks from this category only.', dest='category')
+                'Use dorks from this category only.', dest='category', \
+                metavar='cat')
 
         self.getParser().add_argument('-S', '--severity', help = \
                 'Specify minimum severity(inclusive) dork file to read, \
-                range is [0, 10], defalut: 5.', dest='severity', type=int, \
-                choices=range(1, 11))
+                range is [0, 10]. (defalut: 5)', dest='severity', type=int, \
+                choices=range(1, 11), metavar='N', nargs='?', default=5)
 
-        self.getParser().add_argument( '--only', help = \
-                'Use along with severity, to select only a particular value.',\
-                dest='s_only', action='store_true')
+        self.getParser().add_argument( '--only', help = 'Use supplied \
+                severity as fixed value. Required \'severity\' argument to be \
+                present.', dest='s_only', action='store_true')
 
-        self.getParser().add_argument( '--upper', help ='Use along with \
-                severity, to mark provided value as upper limit (exclusive).',\
-                dest='s_upper', action='store_true')
+        self.getParser().add_argument( '--upper', help ='Mark provided \
+                severity value as upper limit (exclusive). Required \
+                \'severity\' argument to be present.', dest='s_upper', \
+                action='store_true')
 
-        self.getParser().add_argument('-a', '--all', help = \
-                'Use all the dork files to fetch result (overrides --only, \
-                --upper flags).', dest='s_all', action='store_true')
+        self.getParser().add_argument('--all', help = 'Use all the dork \
+                available to fetch result. Beware, this will creates so \
+                much noise. Note: This flag is not respected when present \
+                along with \'severity\'/\'quality\' argument', dest='s_all', \
+                action='store_true')
 
         self.getParser().add_argument('-Q', '--quality', help = \
-                'Use only top severity(>=8) dork files (overrides --only, \
-                --upper flags). ', dest='s_qual', action='store_true')
-
+                'Use only top severity(>=8) dork files to fetch results.\
+                This flag overrides others flags/arguments \
+                (\'severity\'/\'all\') if present.', dest='s_qual', \
+                action='store_true')
 
         self.getParser().add_argument('-r', '--results', help = \
-                'Total results to fetch in one request, default is 30.', \
-                dest='page_size', type=int)
+                'Total results to fetch in one request. (default: 30)', \
+                dest='page_size', type=int, metavar='N', default=30)
 
         self.getParser().add_argument('-t', '--total', help = \
-                'Total results to fetch for each dork, default is 150.', \
-                dest='dork_size', type=int)
+                'Total results to fetch for each dork. (default: 150)', \
+                dest='dork_size', type=int, metavar='N', default=150)
 
         self.getParser().add_argument('-T', '--max', help = \
                 'Maximum results to fetch for all the dorks combined.', \
-                dest='max_results', type=int)
+                dest='max_results', type=int, metavar='N')
 
         self.getParser().add_argument('-m', '--mintime', help = \
                 'Specify minimum sec to wait between requests, If not \
                 specified, default 5 sec range is assumed', dest='min', \
-                type=int)
+                type=int, metavar='N')
 
         self.getParser().add_argument('-M', '--maxtime', help = \
                 'Specify maximum sec to wait between requests, if not \
                 specified, default 5 sec range is assumed.', dest='max', \
-                type=int)
+                type=int, metavar='N')
 
         self.getParser().add_argument('-d', '--delay', help = \
                 'Specify fix delay(in sec), if specified, took priority \
-                over variable delay.', dest='delay', type=int)
+                over variable delay.', dest='delay', type=int, metavar='N')
 
         self.getParser().add_argument('-o', '--output', help = \
-                'Specify output directory', dest='output')
+                'Specify output directory', dest='output', metavar='dir')
 
         self.getParser().add_argument('-j', '--json', help = \
                 'Save output in JSON format only', dest='json', \
                 action="store_true")
 
         self.getParser().add_argument('-R', '--report', help = \
-                'Create Report along with JSON format ouput, default', \
+                'Create Report along with JSON format ouput. (default)', \
                 dest='report', action="store_true")
 
         self.getParser().add_argument('--update', help = \
